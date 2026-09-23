b6fa51cbb9f1dd116bec6465b79722cc3865859a
arda-bit <157163604+arda-bit@users.noreply.github.com>
2026-09-23 19:33:09 +0300

    Added solution for A-Kth Term

---
+++ Atcoder/A - Kth Term.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main(){
+    int arr[]={1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51};
+    int k;
+    scanf("%d",&k);
+    printf("%d",arr[k-1]);
+}
+
